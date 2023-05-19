45ac0818c0c1fa40fbd040c2bb643d10e2a219c4
tsuka8888 <tks.ohtsu.fw77@gmail.com>
2023-05-19 18:03:47 +0900

    ♻️ テキストボックスコンポーネントのスタイルを更新

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -2,9 +2,11 @@ import React from "react"
 import { TextBox } from "./components/TextBox/TextBox"
 
 function App() {
-  return <div className="App">
-    <TextBox />
-  </div>
+  return (
+    <div className="App">
+      <TextBox />
+    </div>
+  )
 }
 
 export default App
