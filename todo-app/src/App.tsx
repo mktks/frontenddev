b7005a9cde4ab077dcd9cfca66060bce7fd50d76
tsuka8888 <tks.ohtsu.fw77@gmail.com>
2023-06-16 18:30:52 +0900

    ✨ textboxのstateを追加

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,12 +1,18 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { TextBox } from "./components/TextBox/TextBox"
 import { Button } from "./components/Button/Button"
 
 function App() {
+  const [inputText, setInputText] = useState("")
+
+  useEffect(() => {
+    console.log("inputTextが変わりました。", inputText)
+  }, [inputText])
+
   return (
     <div className="App">
       <div className="input-container">
-        <TextBox />
+        <TextBox inputText={inputText} onChange={(value) => setInputText(value)} />
         <Button
           onClick={() => {
             console.log("追加ボタンがクリックされました")
--- todo-app/src/components/TextBox/TextBox.tsx
+++ todo-app/src/components/TextBox/TextBox.tsx
@@ -1,5 +1,13 @@
 import "./textBox.css"
 
-export const TextBox = () => {
-  return <input type="text" className="textbox" />
+interface TextBoxProps {
+  /** テキストボックスの中身 */
+  inputText: string
+  /** 値が変わった時の処理 */
+  onChange: (value: string) => void
+}
+
+export const TextBox = (props: TextBoxProps) => {
+  const { inputText, onChange } = props
+  return <input type="text" className="textbox" value={inputText} onChange={(event) => onChange(event.target.value)} />
 }
